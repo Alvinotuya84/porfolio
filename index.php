c01416b0524846a507c4de2f6a3614333a2b7539
David Otuya <otuyad@yahoo.com>
2021-03-12 01:44:11 +0300

    first site

---
+++ index.php
@@ -0,0 +1,285 @@
+<?php include 'contact.php'?>
+
+
+
+
+
+
+<!DOCTYPE html>
+
+
+<html>
+    <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="icon" href="img/background2.jpg">
+        <title>Otuya Alvin</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="default1.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+    </head>
+    <body>
+        <section class="s1" id="s1">
+            <div class="main-container">
+                <div class="greeting-wrapper">
+                       <div class="hero-text">
+                        <h1>I am Otuya Alvin</h1>
+                        <p>And I'm a Software Developer</p>
+                        <a href="#s4"><button onclick="  
+  document.getElementById('response').innerHTML = 'Enter your details here';"
+                            
+                        >Hire me</button></a>
+                        
+                      </div>
+                </div>
+                <div class="intro-wrapper">
+                    <div class="nav-wrapper" id="nav_wrapper">
+                        <div class="logo-wrapper">
+                            <img id="logo" src="https://www.freelogoservices.com/api/main/images/1j+ojFVDOMkX9Wytexe43D6khvaAqxdKnxbIwXs1M3EMoAJtliEphPRq9...87" alt="">
+                            
+                        </div>
+                        <!--<div class="dots-wrapper">-->
+                            <!--<div id="dot-1" class="dots">-->
+                            <!--</div>-->
+                            <!--<div id="dot-2" class="dots">-->
+                            <!--</div>-->
+                            <!--<div id="dot-3" class="dots">-->
+                            <!--</div>-->
+                            <!--<div id="dot-4" class="dots">-->
+                            <!--</div>-->
+                         <!--</div>-->
+                        <ul class="navnav" id="navigation">
+                            <li><a href="#s1">Home</a></li>
+                            <li><a href="#s2">About</a></li>
+                            <li><a href="#s3" class="section-links">Blog</a></li>
+                            <li><a href="#s4">contacts</a></li>
+                            <a href="javascript:void(0);" class="icon" onclick=" return myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+  <div id="myLinks"  class="mobile-links">
+      <a href="#s1">Home</a>
+      <a href="#s2">About</a>
+      <a href="#s3">Blog</a>
+      <a href="#s4">Contacts</a>
+  </div>
+                        </ul>
+                    </div>
+                    <div class="left-column">
+                        
+                    </div>
+                    <div class="right-column">
+                        
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        <section class="s2" id="s2">
+                <div class="about-wrapper">
+                    <div class="about-me" style="text-align: center;">
+                        <h4>More about me</h4>
+                        <p>I build new projects just to tickle my mind and also like teaching others how 
+                            the projects are made</p>
+                            <p>I still do interviews to join large projects that impress me 
+                                while i still make courses for others to get a great team </p>
+                                <hr>
+                                <h4>TOP EXPERTICE</h4>
+                                <p>Full stack developer with primary focus on Django + Laravel+UNO for android: <a href="#">Download resume</a></p>
+                                <div id="skills">
+                                    <ul>
+                                        <li>Python</li>
+                                        <li>Django</li>
+                                        <li>Php</li>
+                                        <li>Laravel</li>
+                                        <li>Java</li>
+                                        <li>Flutter</li>
+            
+            
+                                    </ul>
+                                    <ul>
+                                        <li>Javascript</li>
+                                        <li>HTML/CSS</li>
+                                        <li>MYSQL</li>
+                                        <li>MongoDB</li>
+                                        <li>Node JS</li>
+                                        <li>R</li>
+                                        <li>GOLearn</li>
+                                        <li>React</li>
+            
+                                    </ul>
+                                </div>
+                    </div>
+
+
+                    <div class="social-links">
+                        <a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-google"></a>
+                        <a href="#"  style="color:white;"class="fa fa-github"></a>
+                        <a href="#" class="fa fa-youtube"></a>
+                        <a href="#" class="fa fa-instagram"></a>
+                        <a href="#" class="fa fa-pinterest"></a>
+                        <a href="#" class="fa fa-snapchat-ghost"></a>
+                        <a href="#" class="fa fa-skype"></a>
+                        <a href="#" class="fa fa-android"></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="s3" id="s3">
+            <div class="main-container">
+                <h4 style="text-align: center;">My Projects</h4>
+
+                <div class="post-wrapper">
+                   <div>
+                        <div class="post">
+                            <img class="thumbnail" src="img/chafua.jpg" alt="">
+                            <div class="post-preview">
+                                <h6 class="post-title">Chafua delivery Site</h6>
+                                <p  class="post-intro">I Build a site for my school to ease acess of food between the hostels and the school restaurants </p>
+                                <a href="#">Read more</a>
+
+                            </div>
+                        </div>
+                   </div>
+                    <div>
+                        <div class="post">
+                            <img class="thumbnail" src="img/blog.jpg" alt="">
+                            <div class="post-preview">
+                                <h6 class="post-title">Blog</h6>
+                                <p  class="post-intro"> I Build blog sites for multiple people which aids in adverisement and enterprice business activities</p>
+                                <a href="#">Read more</a>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="post">
+                            <img class="thumbnail" src="img/notes.png" alt="">
+                            <div class="post-preview">
+                                <h6 class="post-title">BBIT Notes gallery</h6>
+                                <p  class="post-intro">I Build a notes gallery system for my class for uploading school notes and downloading the uploaded notes at any time </p>
+                                <a href="#">Read more</a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="s4" id="s4">
+            <div class="main-container">
+                <h4 style="text-align: center;">Find me</h4>
+                <span><p id="response" style="text-align:center;"></p></span>
+                <?php //include 'contact.php' ?>
+                <form id="form" method="POST" name="form" class="contact-form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" onsubmit="return checkUser()">
+                    <label for="">Name</label><br><span class="error" ><?php
+                    echo $name_err;?></span>
+                    <input id="name" class="input-field"type="text"  name="name" onfocus="this.value=''" value="<?php echo $name;?>" required />
+                    <label for="">Email</label><span class="error" ><br><?php
+                    echo $email_err;?></span>
+                    <input id="email"  class="input-field"type="email"  name="email" onfocus="this.value=''" value="<?php echo $email;?>"  required/>
+                    <label for="">subject</label><br><span class="error" ><?php
+                    echo $subject_err;?></span>
+                   <input id="subject"  class="input-field"type="text"  name="subject" onfocus="this.value=''" value="<?php echo $subject;?>" required/>
+                    <label for="">Message</label><br><span class="error"><?php
+                   echo $message_err;?></span>
+                    <textarea id="message"  name="message" class="input-field" cols="30" rows="10" onfocus="this.value=''"><?php echo $message;?></textarea>
+                    <label for="">Tel:</label><br><span class="error" ><?php
+                    echo $phone_err;?></span>
+                    <input id="tel" class="input-field"type="text"  name="phone" onfocus="this.value=''" value="<?php echo $phone;?>" >
+                    <input id="submit-btn" class="buttoninactive" type="submit" value="submit" name="submit">
+                    
+                </form>
+                <span><p id="response"></p></span>
+            </div>
+        </section>
+        <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fa fa-chevron-up" style="font-size:24px"></i>
+</button>
+
+<style>
+    html {
+  scroll-behavior: smooth;
+}
+
+
+        #myBtn {
+  display:none;          
+  position: fixed;
+  bottom: 20px;
+  right: 30px;
+  z-index: 99;
+  border: none;
+  outline: none;
+  background-color:#d1d100;
+  color: white;
+  cursor: pointer;
+  border-radius: 4px;
+
+
+  }
+.fa-github{
+    background-color:black;
+}
+    
+</style>
+<script type="text/javascript" >
+
+$("form").on("submit", function (e) {
+    var dataString = $(this).serialize();
+     
+    $.ajax({
+      type: "POST",
+      url: "contact.php",
+      data: dataString,
+      success: function (response) {
+        alert(response);
+      }
+    });
+
+
+
+ 
+    e.preventDefault();
+});
+//window.scrollTo({ top: 900, behavior: 'smooth' });
+// Select all links with hashes
+
+
+            function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+//Get the button
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
+
+    </body>
+
+</html>
